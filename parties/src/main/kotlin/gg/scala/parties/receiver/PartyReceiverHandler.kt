58671592704faa6635c2dbd88ba9d1db3ff91c31
GrowlyX <growlyxdev@gmail.com>
2022-02-25 18:32:00 -0500

    Configure banana properly

--- parties/src/main/kotlin/gg/scala/parties/receiver/PartyReceiverHandler.kt
+++ parties/src/main/kotlin/gg/scala/parties/receiver/PartyReceiverHandler.kt
@@ -1,10 +1,14 @@
 package gg.scala.parties.receiver
 
+import gg.scala.banana.Banana
 import gg.scala.banana.annotate.Subscribe
+import gg.scala.banana.credentials.BananaCredentials
 import gg.scala.banana.message.Message
+import gg.scala.banana.options.BananaOptions
 import gg.scala.banana.subscribe.marker.BananaHandler
 import gg.scala.flavor.service.Configure
 import gg.scala.flavor.service.Service
+import gg.scala.lemon.Lemon
 import gg.scala.parties.service.PartyService
 import java.util.*
 
@@ -18,7 +22,13 @@ object PartyReceiverHandler : BananaHandler
     @Configure
     fun configure()
     {
+        val banana = Banana(
+            Lemon.instance.credentials,
+            BananaOptions("parties")
+        )
 
+        banana.registerClass(this)
+        banana.subscribe()
     }
 
     @Subscribe("party-update")
