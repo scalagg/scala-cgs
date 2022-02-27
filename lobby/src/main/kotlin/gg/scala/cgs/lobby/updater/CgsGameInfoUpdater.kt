0ec879a6273bff95d7be1d75ee373c01352f4635
GrowlyX <growlyxdev@gmail.com>
2022-02-27 14:03:27 -0500

    Fix logic for CgsGameInfoUpdater.kt

--- lobby/src/main/kotlin/gg/scala/cgs/lobby/updater/CgsGameInfoUpdater.kt
+++ lobby/src/main/kotlin/gg/scala/cgs/lobby/updater/CgsGameInfoUpdater.kt
@@ -7,17 +7,20 @@ import gg.scala.cgs.common.instance.handler.CgsInstanceService
 import gg.scala.cgs.common.states.CgsGameState
 import gg.scala.cgs.lobby.gamemode.CgsGameLobby
 import gg.scala.store.storage.type.DataStoreStorageType
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 /**
  * @author GrowlyX
  * @since 12/4/2021
  */
-object CgsGameInfoUpdater : Thread("CGS - Instance Info Updater")
+object CgsGameInfoUpdater : Runnable
 {
-    val lobbies = mutableSetOf<CgsServerInstance>()
+    var lobbies = mutableSetOf<CgsServerInstance>()
 
-    val gameServers = mutableSetOf<CgsServerInstance>()
-    val gameModeCounts = mutableMapOf<String, Int>()
+    var gameServers = mutableSetOf<CgsServerInstance>()
+    val gameModeCounts = ConcurrentHashMap<String, Int>()
 
     var lobbyTotalCount = 0
     var playingTotalCount = 0
@@ -33,58 +36,49 @@ object CgsGameInfoUpdater : Thread("CGS - Instance Info Updater")
             .firstOrNull()
     }
 
+    fun configure()
+    {
+        val executor = Executors
+            .newSingleThreadScheduledExecutor()
+
+        executor.scheduleAtFixedRate(
+            this, 0L,
+            250L, TimeUnit.MILLISECONDS
+        )
+    }
+
     override fun run()
     {
-        try
+        val engine = CgsGameLobby.INSTANCE
+
+        val instances = CgsInstanceService.service
+            .loadAll(DataStoreStorageType.REDIS)
+            .join()
+
+        lobbies = instances.values
+            .filter { it.type == CgsServerType.LOBBY }
+            .toMutableSet()
+
+        gameServers = instances.values
+            .filter { it.type == CgsServerType.GAME_SERVER }
+            .toMutableSet()
+
+        val playing = instances.values
+            .filter { it.type == CgsServerType.GAME_SERVER }
+            .filter { it.gameServerInfo!!.gameType == CgsGameLobby.INSTANCE.getGameInfo().fancyNameRender }
+
+        for (gameMode in engine.getGameInfo().gameModes)
         {
-            val engine = CgsGameLobby.INSTANCE
-
-            val instances = CgsInstanceService.service
-                .loadAll(DataStoreStorageType.REDIS)
-                .join()
-
-            lobbies.clear()
-            gameServers.clear()
-
-            for (instance in instances)
-            {
-                if (instance.value.type == CgsServerType.LOBBY)
-                {
-                    lobbies.add(instance.value)
-                } else
-                {
-                    gameServers.add(instance.value)
-                }
-            }
-
-            for (gameMode in engine.getGameInfo().gameModes)
-            {
-                gameModeCounts[gameMode.getId()] = instances.values
-                    .filter { it.type == CgsServerType.GAME_SERVER }
-                    .filter { it.gameServerInfo!!.gameType == CgsGameLobby.INSTANCE.getGameInfo().fancyNameRender }
-                    .filter { it.gameServerInfo!!.gameMode == gameMode.getId() }
-                    .sumOf { it.online }
-            }
-
-            playingTotalCount = instances.values
-                .filter { it.type == CgsServerType.GAME_SERVER }
-                .filter { it.gameServerInfo!!.gameType == CgsGameLobby.INSTANCE.getGameInfo().fancyNameRender }
+            gameModeCounts[gameMode.getId()] = playing
+                .filter { it.gameServerInfo!!.gameMode == gameMode.getId() }
                 .sumOf { it.gameServerInfo!!.participants.size }
-
-            playingTotalCount = instances.values
-                .filter { it.type == CgsServerType.LOBBY }
-                .sumOf { it.online }
-        } catch (e: Exception)
-        {
-            e.printStackTrace()
         }
 
-        try
-        {
-            sleep(500L)
-        } catch (e: Exception)
-        {
-            e.printStackTrace()
-        }
+        playingTotalCount = playing
+            .sumOf { it.gameServerInfo!!.participants.size }
+
+        lobbyTotalCount = instances.values
+            .filter { it.type == CgsServerType.LOBBY }
+            .sumOf { it.online }
     }
 }
